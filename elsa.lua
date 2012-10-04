a567b0cdba6867705ce47c3043f9ffc9ce43b914
Markus Stenberg <markus.stenberg@iki.fi>
2012-10-04 12:38:01 +0300

    First iteration which actually seems to work. Shocking.

--- elsa.lua
+++ elsa.lua
@@ -9,21 +9,46 @@
 --       All rights reserved
 --
 -- Created:       Wed Sep 26 23:01:06 2012 mstenber
--- Last modified: Thu Sep 27 12:29:30 2012 mstenber
--- Edit time:     24 min
+-- Last modified: Thu Oct  4 12:37:33 2012 mstenber
+-- Edit time:     50 min
 --
 
 require 'mst'
+require 'ssloop'
+require 'skv'
+require 'elsa_pa'
+
+elsaw = mst.create_class{class='elsaw', mandatory={'c'}}
+
+function elsaw:init()
+   -- something we need to do here?
+end
+
+function elsaw:iterate_lsa(f, criteria)
+   mst.a(criteria, 'criteria mandatory')
+   mst.a(criteria.type, 'criteria.type mandatory')
+   for lsa in elsai_lsas_by_type(self.c, criteria.type)
+   do
+      f(lsa)
+   end
+end
+
+function elsaw:iterate_if(rid, f)
+   for i in elsai_interfaces(self.c)
+   do
+      f(i)
+   end
+end
 
 -- iterate through the interfaces provided by the low-level elsai interface,
 -- wrapping them in tables as we go, and leeching off all
-function elsai_interfaces_iterator(state_c, k)
+function elsai_interfaces_iterator(c, k)
    local i
    if not k
    then
-      i = elsa.elsai_if_get(state_c)
+      i = elsa.elsai_if_get(c)
    else
-      i = elsa.elsai_if_get_next(state_c, getmetatable(k).i)
+      i = elsa.elsai_if_get_next(c, getmetatable(k).i)
    end
    if not i
    then
@@ -34,7 +59,7 @@ function elsai_interfaces_iterator(state_c, k)
    t.name = elsa.elsai_if_get_name(c, i)
    t.index = elsa.elsai_if_get_index(c, i)
    t.priority = elsa.elsai_if_get_priority(c, i)
-   print(i, mst.repr(t))
+   --mst.d(i, mst.repr(t))
    return t
 end
 
@@ -45,6 +70,7 @@ end
 -- similar iterator for lsas by type
 function elsai_lsas_by_type_iterator(state, k)
    local c, type = unpack(state)
+   local l
    if not k
    then
       l = elsa.elsai_get_lsa_by_type(c, type)
@@ -89,9 +115,10 @@ function elsai_interface_table(c)
    return t
 end
 
-function elsa_dispatch()
+function _debug_state()
    local e = elsa.elsa_active_get()
    print('got active', e)
+
    local c = e.client
    print('got client', c)
 
@@ -103,6 +130,39 @@ function elsa_dispatch()
    
    local rid = elsa.elsai_get_rid(c)
    print('got rid', string.format('%x', rid))
+end
+
+local _elsa_pa = false
+
+function get_elsa_pa()
+   local e = elsa.elsa_active_get()
+   mst.d('got active', e)
+   
+   local c = e.client
+   mst.d('got client', c)
+   
+   if not _elsa_pa
+   then
+      local rid = elsa.elsai_get_rid(c)
+      local ew = elsaw:new{c=c}
+      local skv = skv.skv:new{long_lived=true}
+      _elsa_pa = elsa_pa.elsa_pa:new{elsa=ew, skv=skv, rid=rid}
+   else
+      _elsa_pa.c = c
+   end
+   return _elsa_pa
+end
+
+function elsa_dispatch()
+   -- run the event loop also once, in non-blocking mode (this isn't
+   -- really pretty, but oh well) not like the local state
+   -- communication was really that critical
+   ssloop.loop():poll(0)
+
+   -- XXX - should we check if we need to run the PA alg?
+   -- If so, we should consult both SKV state, and LSA state
+   local epa = get_elsa_pa()
+   epa:run()
 end
 
 print('hello from LUA')
--- run_elsa.sh
+++ run_elsa.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ue
+#!/bin/bash -e
 #-*-sh-*-
 #
 # $Id: run_elsa.sh $
@@ -9,9 +9,15 @@
 #       All rights reserved
 #
 # Created:       Thu Sep 27 12:51:34 2012 mstenber
-# Last modified: Thu Sep 27 12:52:13 2012 mstenber
-# Edit time:     1 min
+# Last modified: Thu Oct  4 12:37:10 2012 mstenber
+# Edit time:     3 min
 #
 
 # Propagate LUA_PATH so that required modules can be found more easily..
-sudo LUA_PATH=$LUA_PATH ./bird -d -c bird6.conf.elsa
+
+if [ $* == 1 -a "$1" == "-v" ]
+then
+    sudo ENABLE_MST_DEBUG=1 LUA_PATH=$LUA_PATH valgrind ./bird -d -c bird6.conf.elsa
+else
+    sudo LUA_PATH=$LUA_PATH ./bird -d -c bird6.conf.elsa
+fi
